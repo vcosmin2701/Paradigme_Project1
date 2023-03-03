71e0a5f172167f0a5b677d57a9c0911e6b25d244
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-03-03 17:23:14 +0200

    Creating Pisica object
    New Pisica object in Main class file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Pisica pisica1 = new Pisica();
+        pisica1.mananca();
     }
 }
