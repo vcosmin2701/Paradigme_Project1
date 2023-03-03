0c5fe61f41e96684c3b187b5c05d7175db675adc
Militaru Andreea <andreea.mili20@gmail.com>
2023-03-03 17:25:20 +0200

    Create Animal object
    Added in main class and called the method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Animal animal= new Animal();
+        animal.eatFood();
     }
 }
